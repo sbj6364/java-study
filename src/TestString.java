dc01917d083afbfb8d99bd9417f50dcf75950d12
Seo Byeong Jun <sbj6364@kakao.com>
2021-03-11 21:46:32 +0900

    Create TestString.java

---
+++ src/TestString.java
@@ -0,0 +1,12 @@
+//import java.util.Locale;
+
+public class TestString {
+    public static void main(String[] args) {
+        String a = "Hello Java";
+        System.out.println(a.indexOf("Java"));
+        System.out.println(a.replaceAll("Java","World"));
+        System.out.println(a.substring(0,4));
+        System.out.println(a.toUpperCase());
+        System.out.println(a.toLowerCase());
+    }
+}
